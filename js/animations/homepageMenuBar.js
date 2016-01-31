d8e0336235250e0bfefb8af38a3bbaaf95fc35d6
Antoine Gagné <gagnantoine@gmail.com>
2016-01-30 21:14:14 -0500

    Corrects the javascript and css

--- js/animations/homepageMenuBar.js
+++ js/animations/homepageMenuBar.js
@@ -1,34 +1,40 @@
-$(document).ready(function () {
-    var crossButton = $('.cross');
-    var hamburgerButton = $('.hamburger');
-    hamburgerButton.on('click', function(event) {
-    	event.preventDefault();
-    	var slideOutMenu = $('.navigation-options');
+define([
+    'jquery'
+], function ($) {
+    $(document).ready(function () {
+        var crossButton = $('.cross');
+        var hamburgerButton = $('.hamburger');
+        hamburgerButton.on('click', function (event) {
+            event.preventDefault();
+            var slideOutMenu = $('.navigation-options');
 
-    	slideOutMenu.toggleClass("open");
+            slideOutMenu.toggleClass("open");
 
-    	if (slideOutMenu.hasClass("open")) {
-	    	slideOutMenu.animate({
-		    	left: "0px"
-	    	});
-			crossButton.display('block');
-            hamburgerButton.display('none');
-    	}
-    });
+            if (slideOutMenu.hasClass("open")) {
+                console.log("shit 1");
+                slideOutMenu.animate({
+                    left: "0px"
+                });
+                crossButton.show();
+                hamburgerButton.hide();
+            }
+        });
 
-    crossButton.on('click', function(event) {
-        event.preventDefault();
-        var slideOutMenu = $('.navigation-options');
-        var slideOutMenuWidth = slideOutMenu.width();
+        crossButton.on('click', function (event) {
+            event.preventDefault();
+            var slideOutMenu = $('.navigation-options');
+            var slideOutMenuWidth = slideOutMenu.width();
 
-        slideOutMenu.toggleClass("open");
+            slideOutMenu.toggleClass("open");
 
-        if (!slideOutMenu.hasClass("open")) {
-            slideOutMenu.animate({
+            if (!slideOutMenu.hasClass("open")) {
+                console.log("Shit 2");
+                slideOutMenu.animate({
                     left: -slideOutMenuWidth
                 }, 250);
-            crossButton.display('none');
-            hamburgerButton.display('block');
-        }
+                crossButton.hide();
+                hamburgerButton.show();
+            }
+        });
     });
 });
