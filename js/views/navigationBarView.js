2aec385c4d04bfbe42765be7d733b36e510ada14
JBHarvey <jean.benoit.harvey@outlook.com>
2016-01-31 03:42:18 -0500

    cssed a bit on the menu, added pictures and improved the looks of the searchbar

--- js/views/navigationBarView.js
+++ js/views/navigationBarView.js
@@ -13,28 +13,79 @@ define([
 
     var NavigationBarView = Backbone.View.extend({
 
-        el: $('#menu-content'),
+            el: $('#menu-content'),
 
-        initialize: function () {
-            this.render();
-        },
+            initialize: function () {
+                this.render();
+            },
 
-        render: function () {
+            render: function () {
 
-            var template = Handlebars.compile(navigationBarTemplate);
+                var template = Handlebars.compile(navigationBarTemplate);
 
-            var source = new NavigationBarModel();
-            if (Cookie.get('token') === undefined) {
-                source.disconnect();
-            } else {
-                source.connect(Cookie.get('name'));
+                var source = new NavigationBarModel();
+                if (Cookie.get('token') === undefined) {
+                    source.disconnect();
+                } else {
+                    source.connect(Cookie.get('name'));
+                }
+                var resultNavigationBar = template(source.defaults);
+
+                this.$el.html(resultNavigationBar);
+            },
+
+
+            events: {
+                'click .hamburger': "openHamburgerMenu",
+                'click .cross': "closeHamburgerMenu",
+                'click .go-research': "launchSearchFromButton",
+                'press .search-input': "launchSearchFromInput"
+            },
+
+            slideOutMenu: $('.navigation-options'),
+            crossButton: $('.cross'),
+
+            toggleMenu: function (distance, buttonToShow, buttonToHide) {
+
+            },
+
+
+            openHamburgerMenu: function (eventInfo) {
+                console.log(eventInfo);
+                var hamburgerButton = eventInfo.currentTarget;
+                this.slideOutMenu.toggleClass("open");
+                if (this.slideOutMenu.hasClass("open")) {
+                    this.slideOutMenu.animate({
+                        left: "0px"
+                    });
+                    this.crossButton.show();
+                    hamburgerButton.hide();
+                }
+            },
+
+            closeHamburgerMenu: function () {
+                this.slideOutMenu.toggleClass("open");
+                if (!this.slideOutMenu.hasClass("open")) {
+                    this.slideOutMenu.animate({
+                        left: -this.slideOutMenu.width()
+                    }, 250);
+                    this.crossButton.hide();
+                    this.hamburgerButton.show();
+                }
+            },
+
+            launchSearchFromButton: function (searchGoButton) {
+                console.log(searchGoButton);
+                var input = searchGoButton.target.previousSibling.valueOf();
+                this.launchSearch(input);
+            },
+
+            launchSearch(inputText) {
+                console.log(inputText);
             }
-            var resultNavigationBar = template(source.defaults);
 
-            this.$el.html(resultNavigationBar);
-
-        }
-    });
+        })
+        ;
 
     return NavigationBarView;
 });
