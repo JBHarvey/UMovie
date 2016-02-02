8362265bd0a241d6124ee26f3f1f4de6a28ea53f
JBHarvey <jean.benoit.harvey@outlook.com>
2016-02-02 15:19:35 -0500

    Update before DB class

--- js/collections/movies.js
+++ js/collections/movies.js
@@ -1,3 +1,18 @@
 /**
  * Created by Jean-Benoît on 2016-01-27.
  */
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'jscookie',
+    'models/movieModel'
+], function ($, _, Backbone, Cookie, MovieModel) {
+
+    var Movies = Backbone.collection.extend({
+        model: MovieModel,
+        url: '/movies'
+    });
+
+
+});
