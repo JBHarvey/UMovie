cf6e33a12efd31c55ba238acd5ed211a763e2103
JBHarvey <jean.benoit.harvey@outlook.com>
2016-02-03 01:25:30 -0500

    updatin'

--- js/views/moviesView.js
+++ js/views/moviesView.js
@@ -1,3 +1,33 @@
 /**
  * Created by Jean-Benoît on 2016-02-02.
  */
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'collections/movies',
+    '',
+], function ($, _, Backbone, Movies) {
+
+
+    var MoviesView = Backbone.View.extend({
+
+        el: $('#content'),
+
+        initialize: function (collection) {
+            this.collection = collection;
+            console.log(collection);
+            this.render();
+        },
+
+        render: function () {
+
+            this.collection.each(function(movie) {
+                console.log(movie);
+            });
+            this.$el.html(resultMovie);
+        }
+    });
+    return MovieView;
+
+});
