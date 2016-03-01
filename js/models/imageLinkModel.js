f65c47b6adce5631006cd3504f7b23aaba4e348a
Vincent Emond <vincent.emond.4@ulaval.ca>
2016-03-01 15:22:55 -0500

    adding template for imageLink

---
+++ js/models/imageLinkModel.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Vincent on 16-03-01.
+ */
+
+//Creating a template for the usage of the movies and tv shows
+// thumbnails in their respective pages.
+define([
+    'underscore',
+    'backbone'
+], function (_, Backbone){
+
+    var ImageLinkModel = Backbone.Model.extend({
+        defaults: {
+            "artworkUrl60" : '',
+            "urlReference" : '/movies',
+            "id" : 25,
+            "releaseDate" : '2014-'
+
+        },
+        movie: {
+            "artworkUrl60" : '',
+            "urlReference" : '/movies',
+            "id" : 25,
+            "releaseDate" : ''
+
+        },
+        tvShow: {
+
+            "artworkUrl60" : "http://is3.mzstatic.com/image/pf/us/r30/Video7/v4/b5/17/49/b517492a-91b2-a098-4f75-a8b467cd4fb7/mzl.unbcquai.40x30-75.jpg",
+            "urlReference" : '/tvShows',
+            "id": 279753813,
+            "releaseDate" : '2005-08-28T07:00:00Z'
+        }
+    });
+
+    return ImageLinkModel;
+});
