01d30efe1140c8e9506bcaff679d434ec5c765fa
Vincent Emond <vincent.emond.4@ulaval.ca>
2016-03-02 13:15:39 -0500

    fixing imageLink.html

--- js/models/imageLinkModel.js
+++ js/models/imageLinkModel.js
@@ -12,16 +12,20 @@ define([
     var ImageLinkModel = Backbone.Model.extend({
         defaults: {
             "artworkUrl60" : '',
-            "urlReference" : '/movies',
+            "urlReference" : '/movie',
             "id" : 25,
-            "releaseDate" : '2014-'
+            "releaseDate" : '2014-',
+            "trackName" : "",
+            "collectionName" : ""
 
         },
         movie: {
             "artworkUrl60" : '',
             "urlReference" : '/movies',
             "id" : 25,
-            "releaseDate" : ''
+            "releaseDate" : '',
+            "trackName" : "",
+            "collectionName" : ""
 
         },
         tvShow: {
@@ -29,7 +33,10 @@ define([
             "artworkUrl60" : "http://is3.mzstatic.com/image/pf/us/r30/Video7/v4/b5/17/49/b517492a-91b2-a098-4f75-a8b467cd4fb7/mzl.unbcquai.40x30-75.jpg",
             "urlReference" : '/tvShows',
             "id": 279753813,
-            "releaseDate" : '2005-08-28T07:00:00Z'
+            "releaseDate" : '2005-08-28T07:00:00Z',
+            "trackName" : "",
+            "collectionName" : ""
+
         }
     });
 
