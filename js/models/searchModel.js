eb9d5cf40f6e4531e27b1193d9b4db9a2812aa12
JBHarvey <jean.benoit.harvey@outlook.com>
2016-03-11 16:29:09 -0500

    testing time!

--- js/models/searchModel.js
+++ js/models/searchModel.js
@@ -9,34 +9,55 @@ define([
 
     var SearchModel = Backbone.Collection.extend({
         model: MovieModel,
-        baseURL: 'https://umovie.herokuapp.com/search/',
-        searchType: '',
-        nameParameter: '',
-        limitParameter: '',
-        genreParameter: '',
-        parameterPresence: false,
+        baseURL: 'https://umovie.herokuapp.com/search',
+        parameters: {
+            type: '',
+            name: '',
+            limit: '',
+            genre: '',
+        },
+        addedParameters: 0,
         url: function () {
-            var urlToSend = baseURL;
-            if (parameterPresence) {
-                if (nameParameter != '') {
-                    urlToSend = `${urlToSend}${searchType}`;
-                }
-
-            }
-            return urlToSend;
+            this.addedParameters = 0;
+            var urlToSend = this.parameters.type != '' ? `${this.baseURL}/${this.searchType}` : this.baseURL;
+            let name = this.formatParameter(this.parameters.name);
+            let limit = this.formatParameter(this.parameters.limit);
+            let genre = this.formatParameter(this.parameters.genre);
+            return `${urlToSend}${name}${limit}${genre}`;
         },
 
         parse: function (response) {
             return response.results;
-        }
-        ,
+        },
 
-        setSearchType: function (type) {
-            if (type != 'global') {
+        setSearchType: function(type) {
+            this.parameters.type = type;
+        },
 
-            } else {
-                
+        setSearchLimit: function(limit) {
+            this.parameters.type = limit != 0 ? `limit=${limit}` : '';
+        },
+
+        setSearchGenre: function(genre) {
+            this.parameters.genre= genre != '' ? `genre=${genre}`:'';
+        },
+
+        setSearchName: function(name) {
+            this.parameters.name = name != '' ? `q=${name}`:'';
+        },
+
+        formatParameter: function(parameterToAdd) {
+            let formattedParameter = '';
+            if (parameterToAdd){
+                formattedParameter = `${this.addParamSplitter()}${parameterToAdd}`;
+                this.addedParameters++;
             }
+            return formattedParameter;
+
+        },
+
+        addParamSplitter: function() {
+            return this.addedParameters == 0 ? '?' : '&';
         }
 
 
