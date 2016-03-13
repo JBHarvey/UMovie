82e6ec0e81de54d01e8056d50e6e370ae84c5472
Alexandre Rivest <rivest123@hotmail.com>
2016-03-13 14:55:52 -0500

    Poursuite de Actor

--- js/models/actorModel.js
+++ js/models/actorModel.js
@@ -31,7 +31,47 @@ define([
             "primaryGenreName": "Tribute",
             "primaryGenreId": 100022,
             "radioStationUrl": "https://itunes.apple.com/station/idra.253584821"
+        },
+
+        updateInformationsFromTMDB: function () {
+            var that = this;
+
+            //Appel d'api externe
+            var dataBaseUrl =  "https://api.themoviedb.org/3";
+            var dataBaseApiKey = "?api_key=8e2fb63d78986604185e4448ce8fbaad";
+            var dataBaseImg = "https://image.tmdb.org/t/p/original";
+            var actorName = "&query=" + that.attributes.artistName.split(' ').join('+');
+
+            $.ajax({
+                type: "GET",
+                url: dataBaseUrl + "/search/person" + dataBaseApiKey + actorName,
+                dataType: 'jsonp',
+                jsonCallback: 'test',
+                contentType: 'application/json',
+                success: function(data) {
+                    if(data.results[0]) {
+                        if(data.results[0].profile_path) {
+                            var newImage = dataBaseImg + data.results[0].profile_path;
+                            that.attributes.imgActor = newImage;
+                        }
+                        if (data.results[0].id) {
+                            $.ajax({
+                                type: "GET",
+                                url: dataBaseUrl + "/person/" + data.results[0].id + dataBaseApiKey,
+                                dataType: "jsonp",
+                                jsonpCallback: 'test',
+                                contentType: 'application/json',
+                                success: function (Data) {
+                                    var newBiography = Data.biography;
+                                    that.attributes.biography = newBiography;
+                                }
+                            });
+                        }
+                    }
+                }
+            });
         }
+
     });
 
     return ActorModel;
--- js/views/actorView.js
+++ js/views/actorView.js
@@ -17,65 +17,17 @@ define([
         el: $('#content'),
 
         initialize: function(){
-
-
             this.listenTo(this.model, "change", this.render);
-            this.model.fetch({
-                sucess: function() {
-                    self.render();
-                }
-            });
+            this.model.fetch();
         },
 
         render: function() {
-
-            this.$el.html(this.template(this.model.toJSON()));
-           // var template = Handlebars.compile(actorTemplate);
+            this.model.updateInformationsFromTMDB();
 
             var source = this.model.attributes;
+            var template = Handlebars.compile(actorTemplate);
 
-            //Appel d'api externe
-            var dataBaseUrl =  "https://api.themoviedb.org/3";
-            var dataBaseApiKey = "?api_key=8e2fb63d78986604185e4448ce8fbaad";
-            var dataBaseImg = "https://image.tmdb.org/t/p/original";
-            var actorName = "&query=" + this.model.attributes.artistName.split(' ').join('+');
-            $.ajax({
-                type: "GET",
-                url: dataBaseUrl + "/search/person" + dataBaseApiKey + actorName,
-                dataType: 'jsonp',
-                jsonCallback: 'test',
-                contentType: 'application/json',
-                success: function(data) {
-                    if(data.results[0]) {
-                        if(data.results[0].profile_path) {
-                            document.getElementsByClassName("imgActor").src = dataBaseImg + data.results[0].profile_path;
-                        }
-                        if (data.results[0].id) {
-                            $.ajax({
-                                type: "GET",
-                                url: dataBaseUrl + "/person/" + data.results[0].id + dataBaseApiKey,
-                                dataType: "jsonp",
-                                jsonpCallback: 'test',
-                                contentType: 'application/json',
-                                success: function (Data) {
-                                    document.getElementsByClassName("div").innerHTML = Data.biography;
-                                }
-                            });
-                        }
-                    }
-                }
-            });
-
-
-            var resultActor = template(source);
-
-
-
-            // var template = Handlebars.compile(actorTemplate);
-           // var source = this.model.attributes;
-           //var resultActor = template(source);
-
-            this.$el.html(resultActor);
+            this.$el.html(template(source));
         }
 
     });
