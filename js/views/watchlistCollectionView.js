413d0673a1fed2dc0ab0b9ba8ba7f6432e2c25e2
Vincent Emond <vincent.95@hotmail.com>
2016-03-13 20:48:38 -0400

    call to api working for watchlists

--- js/views/watchlistCollectionView.js
+++ js/views/watchlistCollectionView.js
@@ -1,3 +1,17 @@
-/**
- * Created by Vincent on 16-03-10.
- */
+//Definition of the watchlistCollectionView
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'text!templates/watchlistItem.html',
+    'text!templates/pageHeader.html',
+    'handlebars',
+    'collections/watchlists'
+], function ($, _, Backbone, WatchlistTemplate ,PageHeaderTemplate ,Handlebars, watchlistCollection) {
+        var WatchlistCollectionView = Backbone.View.extend({
+
+    });
+
+    return WatchlistCollectionView;
+});
+
