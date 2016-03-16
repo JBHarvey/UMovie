cc650618de9606e403c741574709123cd233a376
Antoine Gagné <gagnantoine@gmail.com>
2016-03-16 16:00:29 -0400

    Starts working on adding watchlists to the movie view

--- js/views/movieView.js
+++ js/views/movieView.js
@@ -19,11 +19,16 @@ define([
         el: $('#content'),
 
         initialize: function (movieId) {
+            var that = this;
             this.model = new MovieModel({id: movieId});
             this.watchlists = new Watchlists();
             this.listenTo(this.model, "change", this.render);
             this.model.fetch();
-            this.watchlists.fetch();
+            this.watchlists.fetch({
+                success: function () {
+                    that.listenTo(that.watchlists, 'update', that.render);
+                }
+            });
         },
 
         render: function () {
@@ -41,12 +46,54 @@ define([
         },
 
         events: {
-            'click watchlist-select-group': 'addToWatchList'
+            'click .watchlist-select-group': 'addToWatchList',
+            'keyup .new-watchlist-input': 'checkCreateWatchlistInput',
+            'click .submit-new-watchlist': 'createNewWatchlist'
         },
 
         addToWatchList: function (event) {
             "use strict";
+            console.log("Helo");
+            var selectedItem = event.currentTarget;
+            var createWatchlistMenu = $('.new-watchlist');
+            if (selectedItem.value === 'create-watchlist') {
+                createWatchlistMenu.show();
+            } else {
+                createWatchlistMenu.hide();
+            }
+        },
 
+        checkCreateWatchlistInput: function (event) {
+            "use strict";
+            var currentInputValue = event.currentTarget.value;
+            var submitNewWatchlistButton = $('.submit-new-watchlist');
+            if (/^(\w+?\d+?)+$/.test(currentInputValue)) {
+                submitNewWatchlistButton.prop('disabled', false);
+            } else {
+                submitNewWatchlistButton.prop('disabled', true);
+            }
+        },
+
+        createNewWatchlist: function (event) {
+            "use strict";
+
+            var goodInputValue = document.getElementsByClassName('new-watchlist-input');
+            goodInputValue = _.filter(goodInputValue, function (inputValue) {
+                return /^(\w+?\d+?)+$/.test(goodInputValue.value);
+            });
+
+            var watchlist = new Watchlist({
+                'name': goodInputValue.value
+            });
+
+            var that = this;
+            watchlist.save({
+                wait: true,
+                success: function (data) {
+                    console.log(watchlist);
+                    that.watchlists.add(watchlist);
+                }
+            });
         }
     });
     return MovieView;
