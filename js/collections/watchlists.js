519ef76acc04e19e0a96ef9ab943abd1166912c3
Antoine Gagné <gagnantoine@gmail.com>
2016-03-27 15:06:49 -0400

    Changes filter function of the watchlists collection

--- js/collections/watchlists.js
+++ js/collections/watchlists.js
@@ -13,13 +13,23 @@ define([
     var Watchlists = Backbone.Collection.extend({
         model: WatchListModel,
         url: 'https://umovie.herokuapp.com/watchlists',
+        initialize: function (userEmail) {
+            "use strict";
+            if (_.isObject(userEmail)) {
+                this.email = userEmail;
+            } else {
+                this.email = Cookie.get('email');
+            }
+        },
 
         parse: function (response) {
+            var that = this;
+
             var filter = function (data) {
                 "use strict";
                 return _.filter(data, function (model) {
                     var ownerPresent = _.isObject(model.owner);
-                    return ownerPresent ? model.owner.email === Cookie.get('email') : false;
+                    return ownerPresent ? model.owner.email === that.email : false;
                 });
             };
             if (_.isObject(response.results)) {
