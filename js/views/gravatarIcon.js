310f9c1402ee2fae896438bbc38a060e723eeefd
Antoine Gagné <gagnantoine@gmail.com>
2016-03-31 13:36:11 -0400

    Adds gravatar icon

---
+++ js/views/gravatarIcon.js
@@ -0,0 +1,28 @@
+define([
+    'jquery',
+    'backbone',
+    'jscookie',
+    'crypto',
+], function ($, Backbone, Cookie, Crypto) {
+    "use strict";
+
+    return Backbone.View.extend({
+        initialize: function (imageClass) {
+            var encodedEmail;
+            var email = Cookie.get('email');
+            console.log(email);
+            if (typeof email === 'string' || email instanceof String) {
+                encodedEmail = Crypto.MD5(email);
+            } else {
+                encodedEmail = Crypto.MD5('dummyemailaddress@dummy.com');
+            }
+
+            this.getGravatarURL(imageClass, encodedEmail);
+        },
+
+        getGravatarURL: function (imageClass, encodedEmail) {
+            $(imageClass).attr('src', 'https://www.gravatar.com/avatar/' +
+                encodedEmail + '.jpg');
+        },
+    });
+});
