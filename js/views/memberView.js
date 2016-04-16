dca323156a3333c00afd3d5f2874f53fcc821d33
Antoine Gagné <gagnantoine@gmail.com>
2016-04-16 15:52:04 -0400

    Starts refactoring community code

--- js/views/memberView.js
+++ js/views/memberView.js
@@ -14,107 +14,70 @@ define([
     'utils/gravatarIcon',
     'jscookie',
     'views/memberThumbnailView',
-], function ($, _, Backbone, userTemplate, MemberModel, Follow, User, Handlebars, GravatarIcon, Cookie, MemberThumbnailView) {
+], function ($, _, Backbone, UserTemplate, MemberModel, Follow, User, Handlebars, GravatarIcon, Cookie, MemberThumbnailView) {
     'use strict';
 
     var MemberView = Backbone.View.extend({
 
         el: '#content',
         initialize: function () {
-            this.follow = new Follow();
-            this.currentUser = new MemberModel({ id: Cookie.get('id') });
+            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.follow, 'change', this.render);
+
+            this.activeUser = new MemberModel({ id: Cookie.get('id')});
 
             var that = this;
-            this.listenTo(that.currentUser, 'change', that.render);
-            this.listenTo(that.model, 'change', that.render);
-            this.listenTo(that.follow, 'change', that.render);
-            this.currentUser.fetch();
+            var syncRendering = _.after(2, function () {
+                that.render();
+            });
+
+            this.activeUser.fetch({
+                success: syncRendering,
+            });
+
             this.model.fetch({
-                success: that.render(that.model),
+                success: syncRendering,
             });
         },
 
         render: function () {
-            this.model.attributes.isFollowing = this.isFollowing();
-            this.model.attributes.isNotCurrentUser = this.isNotCurrentUser();
-            var source = this.model.attributes;
-            var template = Handlebars.compile(userTemplate);
+            var template = Handlebars.compile(UserTemplate);
 
+            this.model.set('isNotCurrentUser', this.model.get('email') !== Cookie.get('email'));
+            this.model.set('isFollowing', this.isActiveUserFollowing());
+
+            var source = this.model.attributes;
             this.$el.html(template(source));
 
             var gravatarIcon = new GravatarIcon(Cookie.get('email'));
             gravatarIcon.getGravatarURL(`#gravatar-photo-${this.model.id}`);
-
-            this.appendFollowersToHtml();
         },
 
-        appendFollowersToHtml: function () {
-            var followers = this.assembleFollowers();
-            this.$el.append(followers);
-            this.addGravatarIconsToFollowers();
-        },
-
-        assembleFollowers: function () {
-            var followers = '';
-            this.model.attributes.following.forEach(function (member) {
-                member.attributes = member;
-                if (typeof member.gravatarIdName == 'undefined') {
-                    member.gravatarIdName = member.id;
-                }
-
-                var thumbnail = new MemberThumbnailView({ model: member });
-                followers = `${followers}${thumbnail.render()}`;
-            });
-
-            return followers;
-        },
-
-        addGravatarIconsToFollowers: function () {
-            this.model.attributes.following.forEach(function (member) {
-                member.attributes = member;
-                var gravatarIcon = new GravatarIcon(member.email);
-                gravatarIcon.getGravatarURL(`#gravatar-photo-${member.id}`);
-            });
+        isActiveUserFollowing: function () {
+            var that = this;
+            return this.activeUser
+                .get('following')
+                .filter(function (follower) { return follower.id === that.model.id; }).length > 0;
         },
 
         events: {
             'click .follow-unfollow-member': 'followUnfollowMember',
         },
 
-        isFollowing: function () {
-            if (this.currentUser.attributes.following.length > 0) {
-                var currentMemberId = this.model.attributes.id;
-                for (var i = 0; i < this.currentUser.attributes.following.length; i++) {
-                    var userMember = this.currentUser.attributes.following[i];
-                    if (userMember.id === currentMemberId) {
-                        return true;
-                    }
-                }
-            }
-
-            return false;
-        },
-
-        isNotCurrentUser: function () {
-            return this.model.attributes.id !== this.currentUser.attributes.id;
-        },
-
         followUnfollowMember: function (event) {
-            this.$followButton = $('.follow-unfollow-member');
-
-            var follower = new Follow({ id: this.model.attributes.id });
+            var currentButton = event.currentTarget;
 
             var that = this;
-            if (this.$followButton.text().indexOf('Follow') > -1) {
-                follower.save(null, {
+            if (currentButton.innerHTML.replace(/\s/g, '') === 'Follow') {
+                this.activeUser.save(this.model.attributes, {
                     success: function () {
-                        that.$followButton.text('Unfollow');
+                        currentButton.innerHTML = 'Unfollow';
                     },
                 });
             } else {
-                follower.destroy({
+                this.model.destroy({
                     success: function () {
-                        that.$followButton.text('Follow');
+                         currentButton.innerHTML = 'Follow';
                     },
                 });
             }
